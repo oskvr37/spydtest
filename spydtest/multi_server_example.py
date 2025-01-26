6efc84fff050a74b0a72daa53b3b5e5d072c7065
oskvr37 <oskarxpd@gmail.com>
2025-01-26 18:41:09 +0100

    add server download share

--- spydtest/multi_server_example.py
+++ spydtest/multi_server_example.py
@@ -14,15 +14,22 @@ class DownloadTester:
     total_data = 0
     time_start: float = 0.0
     is_timeout = False
+    servers: dict[int, tuple[float, float, Server]] = {}
 
     async def testServerDownload(self, server: Server):
+        self.servers[server.id] = (0.0, 0.0, server)
+
         try:
             while not self.is_timeout:
                 async with self.client.stream(
                     "GET", f"https://{server.host}/download?size={DOWNLOAD_SIZE}"
                 ) as response:
+                    server_start_time = perf_counter()
+                    server_total_data = 0
+
                     async for chunk in response.aiter_bytes():
                         self.total_data += len(chunk)
+                        server_total_data += len(chunk)
 
                         time_elapsed = perf_counter() - self.time_start
                         speed = self.total_data / time_elapsed
@@ -37,6 +44,13 @@ class DownloadTester:
                             self.is_timeout = True
 
                             break
+
+                    current_data, current_time, server = self.servers[server.id]
+                    self.servers[server.id] = (
+                        current_data + server_total_data,
+                        current_time + (perf_counter() - server_start_time),
+                        server,
+                    )
 
         except Exception as e:
             print(f"\nError at {server.name}: {e}")
@@ -59,6 +73,11 @@ async def main():
 
     print(" \u2714")  # tick symbol
     print(f"Downloaded data: {tester.total_data / 1024 / 1024:.2f} MB")
+
+    for server_id in tester.servers:
+        data, time, server = tester.servers[server_id]
+
+        print(f"{server.host} {data / time / 1024 / 1024:.2f} MB/s")
 
 
 if __name__ == "__main__":
